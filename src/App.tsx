e2ee7e4b373b399651c178d2b313edd93d9be8b9
denghongtao1106 <dht_1106@qq.com>
2022-05-08 14:09:54 +0800

    fix: 乾坤子应用提交

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { qiankunGlobalProps } from ".";
 
 function App() {
+  const sendToParent = () => {
+    qiankunGlobalProps.setGlobalState({ name: "jack" });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="App-header" onClick={sendToParent}>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,50 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import "./public-path";
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function render(props: any) {
+  const { container } = props;
+  //   const rootdom = container
+  //   ? container.querySelector("#root")
+  //   : document.querySelector("#root");
+  // const root = ReactDOM.createRoot(rootdom);
+  // root.render(<App />);
+  ReactDOM.render(
+    <App />,
+    container
+      ? container.querySelector("#root")
+      : document.querySelector("#root")
+  );
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+if (!(window as any).__POWERED_BY_QIANKUN__) {
+  render({});
+}
+
+export async function bootstrap() {
+  console.log("[react16] react app bootstraped");
+}
+
+export let qiankunGlobalProps:any = null;
+
+export async function mount(props: any) {
+  qiankunGlobalProps = props;
+  console.log("[react16] props from main framework", props);
+  props.onGlobalStateChange((state: any, prev: any) => {
+    // state: 变更后的状态; prev 变更前的状态
+    console.log("监听到父组件变化");
+
+    console.log(state, prev);
+  });
+  render(props);
+}
+
+export async function unmount(props: any) {
+  const { container } = props;
+  ReactDOM.unmountComponentAtNode(
+    container
+      ? container.querySelector("#root")
+      : document.querySelector("#root")
+  );
+}
